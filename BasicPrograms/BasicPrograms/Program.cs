78aa4ced621208e27545a9ebb551acfceef43c83
poojaamar <pamar6401@gmail.com>
2022-07-17 17:36:25 +0530

    flip heads or tails proram

---
+++ BasicPrograms/BasicPrograms/Program.cs
@@ -0,0 +1,23 @@
+﻿int numOftimes = 10;
+int heads = 0; 
+int tails = 0;
+double headsPercentage, tailsPercentage;
+
+for (int i = 0; i < numOftimes; i++)
+{
+    Random random = new Random();
+    int coinFlip = random.Next(0, 2);
+    if (coinFlip < 0.5)
+    {
+        Console.WriteLine("Its Tails");
+        tails++;
+    }
+    else
+    {
+        Console.WriteLine("Its Heads");
+        heads++;
+    }
+}
+headsPercentage = 100 * heads / numOftimes;
+tailsPercentage = 100 * tails / numOftimes;
+Console.WriteLine("Heads % : " + headsPercentage + " vs Tails % : " + tailsPercentage);
