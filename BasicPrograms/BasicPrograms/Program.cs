1ab13df4ced06a3c11013709c356a9fe3a613aff
poojaamar <pamar6401@gmail.com>
2022-07-18 20:01:31 +0530

    prob 2nd leap year

--- BasicPrograms/BasicPrograms/Program.cs
+++ BasicPrograms/BasicPrograms/Program.cs
@@ -1,23 +1,10 @@
-﻿int numOftimes = 10;
-int heads = 0; 
-int tails = 0;
-double headsPercentage, tailsPercentage;
-
-for (int i = 0; i < numOftimes; i++)
+﻿Console.WriteLine("Enter the year: ");
+int year = int.Parse(Console.ReadLine());
+if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
 {
-    Random random = new Random();
-    int coinFlip = random.Next(0, 2);
-    if (coinFlip < 0.5)
-    {
-        Console.WriteLine("Its Tails");
-        tails++;
-    }
-    else
-    {
-        Console.WriteLine("Its Heads");
-        heads++;
-    }
+    Console.WriteLine(year + " is a Leap Year");
 }
-headsPercentage = 100 * heads / numOftimes;
-tailsPercentage = 100 * tails / numOftimes;
-Console.WriteLine("Heads % : " + headsPercentage + " vs Tails % : " + tailsPercentage);
+else
+{
+    Console.WriteLine(year + " is not a Leap Year");
+}
