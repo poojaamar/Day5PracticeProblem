46229b4ccdb7821aba034c6c33cac9cf957568bc
poojaamar <pamar6401@gmail.com>
2022-07-18 21:31:28 +0530

    prob 8th vowel and consonant

--- BasicPrograms/BasicPrograms/Program.cs
+++ BasicPrograms/BasicPrograms/Program.cs
@@ -1,6 +1,11 @@
-﻿String num = args[0];
-int n = int.Parse(num);
-for (int i = 1; i <= n; i++)
+﻿char ah;
+Console.WriteLine("Enter the letter:");
+ah = Char.Parse(Console.ReadLine());
+if ((ah == 'a') || (ah == 'e') || (ah == 'i') || (ah == 'o') || (ah == 'u') || (ah == 'A') || (ah == 'E') || (ah == 'I') || (ah == 'O') || (ah == 'U'))
 {
-    Console.WriteLine(Math.Pow(2, i));
+    Console.WriteLine(ah + " is a Vowel.");
+}
+else
+{
+    Console.WriteLine(ah + " is a Consonant.");
 }
